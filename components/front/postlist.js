31dd7b34da5a4fa2694b8c2c9524e4ffb4a1893b
wojter <wojtektro2@gmail.com>
2023-01-15 16:51:23 +0100

    Add main page get post from db, fix date format

--- components/front/postlist.js
+++ components/front/postlist.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { PhotographIcon } from "@heroicons/react/outline";
 import CategoryLabel from "./catogory";
+import dayjs from "dayjs";
 
 const PostList = ({ post, aspect, preloadImage }) => {
   const imageProps = { src: "/sample_photo.jpeg" };
@@ -13,7 +14,7 @@ const PostList = ({ post, aspect, preloadImage }) => {
           ${aspect === "landscape" ? "aspect-video" : "aspect-square"}`}
         >
           {/* <Link href={`/post/${post.slug.current}`}> */}
-          <Link href={`/post/`}>
+          <Link href={`/post/`+ post._id}>
             <a>
               {imageProps ? (
                 <Image
@@ -54,31 +55,6 @@ const PostList = ({ post, aspect, preloadImage }) => {
             </span>
           </Link>
         </h2>
-
-        {/* <CategoryLabel categories={post.categories} />
-        <h2 className="mt-2 text-lg font-semibold tracking-normal text-brand-primary dark:text-white">
-          <Link href={`/post/${post.slug.current}`}>
-            <span
-              className="     bg-gradient-to-r from-green-200 to-green-100 dark:from-purple-800 dark:to-purple-900
-            bg-[length:0px_10px]
-            bg-left-bottom
-            bg-no-repeat
-            transition-[background-size]
-            duration-500
-            hover:bg-[length:100%_3px] group-hover:bg-[length:100%_10px]"
-            >
-              {post.title}
-            </span>
-          </Link>
-        </h2>
-
-        <div className="hidden">
-          {post.excerpt && (
-            <p className="mt-2 text-sm text-gray-500 dark:text-gray-400 line-clamp-3">
-              <Link href={`/post/${post.slug.current}`}>{post.excerpt}</Link>
-            </p>
-          )}
-        </div> */}
         <div className="flex items-center mt-3 space-x-3 text-gray-500 dark:text-gray-400">
           <div className="flex items-center gap-3">
             <span className="text-sm">{post?.author}</span>
@@ -88,43 +64,10 @@ const PostList = ({ post, aspect, preloadImage }) => {
           </span>
           <time
             className="text-sm"
-            dateTime={post?.publishedAt || post?._createdAt}
-          >{post?.created}
+            dateTime={post?.created}
+          >{dayjs(post?.created).format("DD/MM/YYYY")}
           </time>
         </div>
-
-        {/* <div className="flex items-center mt-3 space-x-3 text-gray-500 dark:text-gray-400">
-          <div className="flex items-center gap-3">
-            <div className="relative flex-shrink-0 w-5 h-5">
-              {post.author.image && (
-                <Image
-                  src={AuthorimageProps.src}
-                  blurDataURL={AuthorimageProps.blurDataURL}
-                  loader={AuthorimageProps.loader}
-                  objectFit="cover"
-                  layout="fill"
-                  alt={post?.author?.name}
-                  placeholder="blur"
-                  sizes="30px"
-                  className="rounded-full"
-                />
-              )}
-            </div>
-            <span className="text-sm">{post.author.name}</span>
-          </div>
-          <span className="text-xs text-gray-300 dark:text-gray-600">
-            &bull;
-          </span>
-          <time
-            className="text-sm"
-            dateTime={post?.publishedAt || post._createdAt}
-          >
-            {format(
-              parseISO(post?.publishedAt || post._createdAt),
-              "MMMM dd, yyyy"
-            )}
-          </time>
-        </div> */}
       </div>
     </>
   );
--- pages/main.js
+++ pages/main.js
@@ -2,29 +2,34 @@ import Footer from "../components/front/footer";
 import Navbar from "../components/front/navbar";
 import PostList from "../components/front/postlist";
 import Container from "../components/front/container";
-import { getPosts, getPostCategories } from "../lib/front/load-posts";
-
+import { getPosts } from "../lib/front/load-posts";
 
 const Main = (props) => {
-  const post = {
-    category: "Technology",
-    title:
-      "Architectural Engineering Wonders of the modern era for your Inspiration",
-    author: "Mario Sanchez",
-    createdAt: "12.12.2022",
-  };
 
   return (
     <div>
       <Navbar />
       <Container>
         <div className="grid gap-10 lg:gap-10 md:grid-cols-2 ">
-          <PostList post={props.posts[0]} aspect="landscape" preloadImage={true} />
-          <PostList post={post} aspect="landscape" preloadImage={true} />
+          {props.posts.slice(0, 2).map((post) => (
+            <PostList
+              key={post._id}
+              post={post}
+              aspect="landscape"
+              preloadImage={true}
+            />
+          ))}
         </div>
         <div className="grid gap-10 mt-10 lg:gap-10 md:grid-cols-2 xl:grid-cols-3 ">
-          <PostList post={post} aspect="landscape" preloadImage={true} />
-          <PostList post={post} aspect="landscape" preloadImage={true} />
+          {props.posts.slice(2).map((post) => (
+            <PostList
+            key={post._id}
+            post={post}
+            aspect="landscape"
+            preloadImage={true}
+          />
+          ))}
+
         </div>
       </Container>
       <Footer />
@@ -37,10 +42,10 @@ export async function getStaticProps() {
   // const posts = await res.json()
   // console.log(posts)
   const posts = await getPosts();
-  console.log(posts)
+
   return {
     props: { posts },
-  }
+  };
 }
 
 export default Main;
